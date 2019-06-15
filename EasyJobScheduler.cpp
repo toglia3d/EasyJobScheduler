f332e850b55a0198cd4d2870dbef6fef07e75b6d
Alberto Toglia <alberto.toglia@green-hippo.com>
2019-06-15 15:37:34 +0100

    Validating tasks for circular dependencies

--- EasyJobScheduler.cpp
+++ EasyJobScheduler.cpp
@@ -1,16 +1,20 @@
-
 #include <string>
 #include <unordered_map>
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <cctype>
 
-struct Task
+
+class Task
 {
+public:
     std::string m_name;
     std::string m_commands;
-    std::vector<std::string> m_depenencies;
+    std::vector<std::string> m_dependencies;
 };
+
+using task_map = std::unordered_map<std::string, Task>;
 
 class Job
 {
@@ -27,7 +31,7 @@ static std::vector<std::string> split(std::string const& argument, const char se
     {
         const auto next_token_index = argument.find_first_of(separator, token_index);
         auto word = argument.substr(token_index, next_token_index - token_index);
-        word.erase(std::remove_if(word.begin(), word.end(), isspace), word.end());
+        word.erase(std::remove_if(word.begin(), word.end(), ::isspace), word.end());
         result.push_back(word);
         token_index = next_token_index;
     }
@@ -35,17 +39,17 @@ static std::vector<std::string> split(std::string const& argument, const char se
 }
 
 
-Task readFile(std::string const& path)
+Task read_file(std::string const& path, std::string& error)
 {
     Task task;
     std::string line;
-    std::ifstream myfile(path);
-    if (myfile.is_open())
+    std::ifstream my_file(path);
+    if (my_file.is_open())
     {
-        getline(myfile, task.m_name);
+        getline(my_file, task.m_name);
         std::string last_line;
         int lineCount = 1;
-        while (getline(myfile, line))
+        while (getline(my_file, line))
         {
             last_line = line;
             task.m_commands += line;
@@ -55,47 +59,109 @@ Task readFile(std::string const& path)
         if(lineCount > 2)
         {
             task.m_commands.erase(task.m_commands.size() - last_line.size(), last_line.size());
-            task.m_depenencies = split(last_line, ',');
+            task.m_dependencies = split(last_line, ',');
         }
 
-        myfile.close();
+        my_file.close();
     }
     else
     {
-        std::cout << "Unable to open file";
+        error = "Unable to open file";
     }
 
     return task;
 }
 
-bool checkTasks(std::vector<Task> const& tasks, std::string& error)
+void recurseCheckDependencies(
+    Task const& current_task, 
+    task_map const& tasks,
+    std::unordered_map<std::string, int>& visited,
+    int& current_depth,
+    bool& circular_dependency, 
+    std::string& trace_str)
 {
-    for (const auto& taskA : tasks)
+    if (circular_dependency)
     {
-        for (const auto& dependency : taskA.m_depenencies)
+        return;
+    }
+
+    trace_str += current_task.m_name + "->";
+    visited[current_task.m_name] = current_depth;
+
+    current_depth++;
+
+    for (const auto& dependency_name : current_task.m_dependencies)
+    {
+        auto dependency_task_itr = tasks.find(dependency_name);
+        if (dependency_task_itr != tasks.end())
         {
-
-            if (dependency == taskA.m_name)
+            auto& dependency_task = dependency_task_itr->second;
+            auto visited_depth_itr = visited.find(dependency_task.m_name);
+            if (visited_depth_itr != visited.end())
             {
-                std::cout << taskA.m_name << " has a circular Depency" << std::endl;
-                return false;
-            }
-
-            bool found = false;
-            for (auto const& taskB : tasks)
-            {
-                if (taskB.m_name == dependency)
+                const auto visited_depth = visited_depth_itr->second;
+                if (visited_depth < current_depth)
                 {
-                    found = true;
+                    circular_dependency = true;
+                    trace_str += dependency_task.m_name;
                     break;
                 }
             }
 
-            if (!found)
+            recurseCheckDependencies(dependency_task, tasks, visited, current_depth, circular_dependency, trace_str);
+        }
+    }
+
+    current_depth--;
+}
+
+bool has_circular_dependency(
+    Task const& task, 
+    task_map const& tasks, 
+    std::string& error)
+{
+    std::unordered_map<std::string, int> visited;
+    bool circular_dependency_found = false;
+    std::string trace_string;
+    int depth = 0;
+
+    recurseCheckDependencies(task, tasks, visited, depth, circular_dependency_found, trace_string);
+
+    if (circular_dependency_found)
+    {
+        error = trace_string;
+    }
+
+    return circular_dependency_found;
+}
+
+
+bool can_run_task(task_map const& tasks, std::string& error)
+{
+    for (const auto& task_itr : tasks)
+    {
+        auto& task = task_itr.second;
+
+        for (const auto& dependency : task.m_dependencies)
+        {
+            if (dependency == task.m_name)
             {
-                std::cout << "Unable to find dependency " << dependency;
+                error = "Error: \""+ task.m_name + "\" depends on itself.";
                 return false;
             }
+
+            if (tasks.find(dependency) == tasks.end())
+            {
+                error = "Error: Unable to find dependency: \"" + dependency+"\"";
+                return false;
+            }
+        }
+
+        std::string dependency_tree; 
+        if (has_circular_dependency(task, tasks, dependency_tree))
+        {
+            error = "Error! Found circular dependency: \n" + dependency_tree;
+            return false;
         }
     }
 
@@ -103,43 +169,35 @@ bool checkTasks(std::vector<Task> const& tasks, std::string& error)
 }
 
 
-void runDependencies(Task& currentTask, std::vector<Task>& tasks)
+
+void runDependencies(Task const& current_task, task_map const& tasks, std::unordered_map<std::string, bool>& executed_tasks)
 {
-    for (const auto& dependency : currentTask.m_depenencies)
+    for (const auto& dependency_name : current_task.m_dependencies)
     {
-        for (auto& task : tasks)
+        auto dependency_task_itr = tasks.find(dependency_name);
+        if (dependency_task_itr != tasks.end())
         {
-            if (dependency == task.m_name)
+            auto& dependency_task = dependency_task_itr->second;
+            if (!dependency_task.m_dependencies.empty())
             {
-                if (task.m_depenencies.empty())
-                {
-                    std::cout << task.m_commands << std::endl;
-                    break;
-                }
-                else
-                {
-                    runDependencies(task, tasks);
-                }
+                runDependencies(dependency_task, tasks, executed_tasks);
+            }
+
+            if (executed_tasks.find(dependency_task.m_name) == executed_tasks.end())
+            {
+                std::cout << dependency_task.m_commands << std::endl;
+                executed_tasks[dependency_task.m_name] = true;
             }
         }
     }
-
-    std::cout << currentTask.m_commands << std::endl;
 }
 
-void run(std::string const& name, std::vector<Task>& tasks)
+void run(Task const& task, task_map const& tasks)
 {
-
-    for (auto& task : tasks)
-    {
-        if (task.m_name == name)
-        {
-            runDependencies(task, tasks);
-        }
-    }
+    std::unordered_map<std::string, bool> executed_tasks;
+    runDependencies(task, tasks, executed_tasks);
+    std::cout << task.m_commands << std::endl;
 }
-
-
 
 int main(int argc, char *argv[])
 {
@@ -155,22 +213,36 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    std::vector<Task> tasks;
+    task_map tasks;
+    std::string error;
 
-    for (int i = 2; i< arguments.size(); ++i)
+    for (int i = 2; i<arguments.size(); ++i)
     {
-        tasks.emplace_back(readFile(arguments[i]));
+        auto task = read_file(arguments[i], error);
+
+        if(error.empty())
+        {
+            tasks[task.m_name] = task;
+        }
+        else
+        {
+            std::cout << error << std::endl;
+            break;
+        }
     }
 
-    std::string error;
-    if (checkTasks(tasks, error))
+    if (can_run_task(tasks, error))
+    {
+        auto& main_task = tasks[arguments[1]];
+        run(main_task, tasks);
+    }
+    else
     {
         std::cout << error << std::endl;
     }
 
-    run(arguments[1], tasks);
-
     getchar();
+    
     
     return 0;
 }
